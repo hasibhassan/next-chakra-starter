c4cc9595f817e05770d62387b27575bf95dfc9ac
hqhas <67892792+hqhas@users.noreply.github.com>
2020-12-29 12:49:57 -0600

    fix: 🎨

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>Hello World!</h1>
     </div>
-  );
+  )
 }
